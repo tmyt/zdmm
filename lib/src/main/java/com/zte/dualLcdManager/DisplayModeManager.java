1fc6ca99ebad2b4aed26355adbd3abc46ac4e3ca
yutaka <shigure@refy.net>
2018-04-02 03:42:39 +0900

    add files

---
+++ lib/src/main/java/com/zte/dualLcdManager/DisplayModeManager.java
@@ -0,0 +1,173 @@
+package com.zte.dualLcdManager;
+
+import android.content.ComponentName;
+import android.content.Context;
+
+public class DisplayModeManager
+{
+    public static final int DISPLAY_MODE_DOCKED = 4;
+    public static final int DISPLAY_MODE_MIRROR = 8;
+    public static final int DISPLAY_MODE_NULL = 0;
+    public static final int DISPLAY_MODE_SINGLE = 1;
+    public static final int DISPLAY_MODE_ZOOM = 2;
+
+    public static final String getConvertOrientationWhiteList()
+    {
+        throw new RuntimeException();
+    }
+
+    public static int getCurrentDisplayMode()
+    {
+        throw new RuntimeException();
+    }
+
+    public static final String getHalfWidthWhiteList()
+    {
+        throw new RuntimeException();
+    }
+
+    public static DisplayModeManager getInstance(Context paramContext)
+    {
+        throw new RuntimeException();
+    }
+
+    public static final String getNoPauseWhiteList()
+    {
+        throw new RuntimeException();
+    }
+
+    public static String initPackagesList(String paramString)
+    {
+        throw new RuntimeException();
+    }
+
+    public static boolean isExtendedBufferMode()
+    {
+        throw new RuntimeException();
+    }
+
+    public static final boolean isInZoomRevertOrientationWhiteList(String paramString)
+    {
+        throw new RuntimeException();
+    }
+
+    public static boolean isSingleBufferMode()
+    {
+        throw new RuntimeException();
+    }
+
+    public int getCurrentMode()
+    {
+        throw new RuntimeException();
+    }
+
+    public int getHallSensorStatus()
+    {
+        throw new RuntimeException();
+    }
+
+    public ComponentName getStackTopActivity(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public int getSwitchModeDisable()
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean handleRestrictionAppsIfNeeded(String paramString)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean handleSwitchModeByUnResizable(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean isDisplayModeSwitching(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean isTopActivityInSingleModeWhiteList()
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean isTopTaskResizable()
+    {
+        throw new RuntimeException();
+    }
+
+    public String noPauseList()
+    {
+        throw new RuntimeException();
+    }
+
+    public void performTraversalInTransaction()
+    {
+        throw new RuntimeException();
+    }
+
+    public void recoverSecondaryDisplayDeviceBrightness()
+    {
+        throw new RuntimeException();
+    }
+
+    public int setMainDisplayDeviceBrightness(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public void setSecondaryDisplayDevicePowerState(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public void setSwitchModeDisable(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public void swapDockedAndFullscreenStack()
+    {
+        throw new RuntimeException();
+    }
+
+    public void switchMessage(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean switchMode(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean switchModeBySpecialApp(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean switchModeNotCheckSwitchModeBlackList(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+
+    public void switchSingleModeByCamera()
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean willRelaunchIfSwitch()
+    {
+        throw new RuntimeException();
+    }
+
+    public boolean willRelaunchIfSwitch(int paramInt)
+    {
+        throw new RuntimeException();
+    }
+}
